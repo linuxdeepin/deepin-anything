c9fd0701e72b87ae3426cbdabd941e840c214b94
dxnu <ymiwli@gmail.com>
2024-11-20 13:21:10 +0800

    Remove tracked directories from repository

--- debian/tmp/usr/src/deepin-anything-0.0/arg_extractor.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2021 UOS Technology Co., Ltd.
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#pragma once
-
-unsigned long get_arg(struct pt_regs* regs, int n);
-
